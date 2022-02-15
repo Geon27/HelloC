fb0e38d5138924041d575fe93cfcc236faee9b8e
Geon Yuk <89954838+Geon27@users.noreply.github.com>
2022-02-16 00:33:34 +0900

    Add files via upload

---
+++ ex3-3.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+	double weight, height;
+
+	printf("체중(kg) 입력 : ");
+	scanf("%lf", &weight);
+	printf("키(m) 입력 : ");
+	scanf("%lf", &height);
+
+	printf("BMI: %f\n", weight / (height * height));
+}
