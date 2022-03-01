3786462fd738ef4084516cf1f43a75d7621c2696
Geon Yuk <89954838+Geon27@users.noreply.github.com>
2022-03-02 02:19:02 +0900

    Add files via upload

---
+++ ex4-24.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main() {
+	int score;
+
+	printf("점수 입력 : ");
+	scanf("%d", &score);
+	if (score < 0 || score > 100) {
+		printf("잘못 입력하셨습니다.\n");
+	}
+	else if (score >= 90) {
+		printf("A\n");
+	}
+	else if (score >= 80) {
+		printf("B\n");
+	}
+	else if (score >= 70) {
+		printf("C\n");
+	}
+	else if (score >= 60) {
+		printf("D\n");
+	}
+	else {
+		printf("F\n");
+	}
+}
