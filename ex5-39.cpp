ff1bf1a5f91b0d5a2ef7d25b9ecc1b3c1b9fcfcd
Geon Yuk <89954838+Geon27@users.noreply.github.com>
2022-03-26 00:12:21 +0900

    Add files via upload

---
+++ ex5-39.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+	int arr[3][3] = { 0 };
+
+	printf("%d\n", &arr);
+	printf("%d\n", arr);
+	printf("%d\n", *arr);
+
+	printf("%d\n", &arr[0]);
+	printf("%d\n", arr[0]);
+	printf("%d\n", *arr[0]);
+
+	printf("%d\n", &arr[0][0]);
+	printf("%d\n", arr[0][0]);
+}
