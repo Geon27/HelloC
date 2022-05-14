cb498a87320521286547b8c7713736012d084fa4
Geon Yuk <89954838+Geon27@users.noreply.github.com>
2022-05-14 11:59:42 +0900

    Add files via upload

---
+++ ex10-10.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class Complex {
+public:
+	Complex(double real_ = 0, double imag_ = 0) {
+		real = real_;
+		imag = imag_;
+	}
+
+	double GetReal() {
+		return real;
+	}
+	void SetReal(double real_) {
+		real = real_;
+	}
+
+private:
+	double real;
+	double imag;
+};
+
+int main() {
+	Complex c1;
+	Complex c2 = Complex(2, 3);
+	Complex c3(2, 3);
+
+	cout << "c1 = " << c1.GetReal() << ", " << c1.GetImag() << endl;
+	cout << "c2 = " << c2.GetReal() << ", " << c2.GetImag() << endl;
+	cout << "c3 = " << c3.GetReal() << ", " << c2.GetImag() << endl;
+}
