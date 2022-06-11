8cea7912cae24278f8812d160117929e2c57a373
Geon Yuk <89954838+Geon27@users.noreply.github.com>
2022-06-12 02:48:13 +0900

    Add files via upload

---
+++ ex13-3.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+template<typename T>
+T getArraySum(const T arr[], int n) {
+	T sum = arr[0];
+	for (int i = 1; i < n; i++) {
+		sum += arr[i];
+	}
+	return sum;
+}
+
+template<>
+string getArraySum(const string arr[], int n) {
+	string sum = arr[0];
+	for (int i = 1; i < n; i++) {
+		sum += " " + arr[i];
+	}
+	return sum;
+}
+
+int main() {
+	int iarr[5] = { 3, 1, 4, 1, 5 };
+	string sarr[3] = { "hello", "world", "doodle" };
+
+	int isum = getArraySum(iarr, 5);
+	string ssum = getArraySum(sarr, 3);
+
+	cout << isum << endl;
+	cout << ssum << endl;
+}
