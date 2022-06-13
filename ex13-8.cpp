0171ab70f1fb18c96ba222a9d9dc92b5787e4f40
Geon Yuk <89954838+Geon27@users.noreply.github.com>
2022-06-14 01:14:04 +0900

    Add files via upload

---
+++ ex13-8.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int fact(int n) {
+	if (n < 1) throw to_string(n) + ": 자연수가 아닙니다.";
+	if (n == 1) return 1;
+	return n * fact(n - 1);
+}
+
+int main() {
+	int n, r;
+
+	while (true) {
+		try {
+			cout << "자연수 2개 입력 : ";
+			cin >> n >> r;
+			cout << comb(n, r) << endl;
+		}
+		catch (const string& e) {
+			cout << e << endl;
+		}
+	}
+}
